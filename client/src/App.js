f70d4ba7ff0d37d1269886f75234983aa404f96f
Agostina Gavilan <agosgavilan3@gmail.com>
2022-04-01 15:54:10 -0300

    Changes on details component and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import { Route, Routes } from "react-router-dom"
 import './App.css';
 import Home from "../src/components/Home/Home.jsx"
-
 import Form from "../src/components/Form/Form";
-
 import NavBar from "./components/NavBar/NavBar";
+import Details from "./components/Details/Details";
 
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/home" element={<Home/>}/>
         <Route exact path="/form" element={<Form/>}/>
+        <Route exact path="/movies/:id" element={<Details/>} />
       </Routes>
     </div>
   );
--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { details } from "../../redux/actions";
-import img from '../imagenes/background-popcorn-film-wallpaper-preview.jpg'
+import image from '../../assets/background-popcorn-film-wallpaper-preview.jpg'
 
 const Details = () => {
     const {id} = useParams()
@@ -15,13 +15,14 @@ const Details = () => {
 
     return (
         <div>
-            <h1>{movieDetail.name}</h1>
-            <img src={movieDetail.image ? movieDetail : img} alt={`${movieDetail.name}'s`} width="300px" height="150px"/>
-            <p>{movieDetail.rating}</p>
-            <p>{movieDetail.genres}</p>
-            <p>{movieDetail.released}</p>
-            <p>{movieDetail.description}</p>
-            <p>{movieDetail.platforms}</p>
+            <h1>{movieDetail.title}</h1>
+            <img src={movieDetail.img ? movieDetail.img : image} alt={`${movieDetail.name}'s`} width="300px" height="150px"/>
+            <p>Vote: {movieDetail.vote_average}</p>
+            <p>Genres: {movieDetail.Genres?.map(g => g.name).join(' | ')}</p>
+            <p>Release: {movieDetail.release_date}</p>
+            <p>Description: {movieDetail.overview}</p>
+            <p>Price: u$d{movieDetail.price}</p>
+            <p>Actores: {movieDetail.Actors?.map(a => a.name).join(', ')}</p>
         </div>
     )
 }
